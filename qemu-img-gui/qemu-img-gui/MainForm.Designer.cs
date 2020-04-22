f2672fadce5a53c3a1d615570c3cbb45955ed975
Muhamad Samsudin <muhamad.sams@cito.co.id>
2020-04-22 17:20:32 +0700

    add convert from physical drive

--- qemu-img-gui/qemu-img-gui/MainForm.Designer.cs
+++ qemu-img-gui/qemu-img-gui/MainForm.Designer.cs
@@ -36,14 +36,25 @@
             this.btnBrowseOutput = new System.Windows.Forms.Button();
             this.cbDynamic = new System.Windows.Forms.CheckBox();
             this.btnStart = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cmbOutput = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmbSourcePhysical = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnStartPhysical = new System.Windows.Forms.Button();
+            this.txtOutputPhysical = new System.Windows.Forms.TextBox();
+            this.btnBrowseOutputPhysical = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 89);
+            this.label1.Location = new System.Drawing.Point(-1, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 0;
@@ -51,14 +62,14 @@
             // 
             // txtSourceFile
             // 
-            this.txtSourceFile.Location = new System.Drawing.Point(118, 86);
+            this.txtSourceFile.Location = new System.Drawing.Point(105, 22);
             this.txtSourceFile.Name = "txtSourceFile";
             this.txtSourceFile.Size = new System.Drawing.Size(346, 20);
             this.txtSourceFile.TabIndex = 2;
             // 
             // btnBrowse
             // 
-            this.btnBrowse.Location = new System.Drawing.Point(470, 84);
+            this.btnBrowse.Location = new System.Drawing.Point(457, 20);
             this.btnBrowse.Name = "btnBrowse";
             this.btnBrowse.Size = new System.Drawing.Size(75, 23);
             this.btnBrowse.TabIndex = 3;
@@ -69,7 +80,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 115);
+            this.label2.Location = new System.Drawing.Point(-1, 51);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(39, 13);
             this.label2.TabIndex = 0;
@@ -77,14 +88,14 @@
             // 
             // txtOutput
             // 
-            this.txtOutput.Location = new System.Drawing.Point(118, 112);
+            this.txtOutput.Location = new System.Drawing.Point(105, 48);
             this.txtOutput.Name = "txtOutput";
             this.txtOutput.Size = new System.Drawing.Size(346, 20);
             this.txtOutput.TabIndex = 4;
             // 
             // btnBrowseOutput
             // 
-            this.btnBrowseOutput.Location = new System.Drawing.Point(470, 110);
+            this.btnBrowseOutput.Location = new System.Drawing.Point(457, 46);
             this.btnBrowseOutput.Name = "btnBrowseOutput";
             this.btnBrowseOutput.Size = new System.Drawing.Size(75, 23);
             this.btnBrowseOutput.TabIndex = 5;
@@ -106,7 +117,7 @@
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(221, 151);
+            this.btnStart.Location = new System.Drawing.Point(208, 87);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(75, 23);
             this.btnStart.TabIndex = 6;
@@ -114,21 +125,21 @@
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
-            // comboBox1
+            // cmbOutput
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.cmbOutput.FormattingEnabled = true;
+            this.cmbOutput.Items.AddRange(new object[] {
             "vpc",
             "vhdx",
             "vmdk",
             "qcow2",
             "raw",
             "vdi"});
-            this.comboBox1.Location = new System.Drawing.Point(118, 37);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(205, 21);
-            this.comboBox1.TabIndex = 1;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.cmbOutput.Location = new System.Drawing.Point(118, 37);
+            this.cmbOutput.Name = "cmbOutput";
+            this.cmbOutput.Size = new System.Drawing.Size(205, 21);
+            this.cmbOutput.TabIndex = 1;
+            this.cmbOutput.SelectedIndexChanged += new System.EventHandler(this.cmbOutput_SelectedIndexChanged);
             // 
             // label3
             // 
@@ -139,25 +150,119 @@
             this.label3.TabIndex = 0;
             this.label3.Text = "Output option";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtSourceFile);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.txtOutput);
+            this.groupBox1.Controls.Add(this.btnBrowseOutput);
+            this.groupBox1.Controls.Add(this.btnBrowse);
+            this.groupBox1.Location = new System.Drawing.Point(12, 79);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(532, 121);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "From Image";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmbSourcePhysical);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.btnStartPhysical);
+            this.groupBox2.Controls.Add(this.txtOutputPhysical);
+            this.groupBox2.Controls.Add(this.btnRefresh);
+            this.groupBox2.Controls.Add(this.btnBrowseOutputPhysical);
+            this.groupBox2.Location = new System.Drawing.Point(12, 206);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(532, 121);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "From Physical";
+            // 
+            // cmbSourcePhysical
+            // 
+            this.cmbSourcePhysical.FormattingEnabled = true;
+            this.cmbSourcePhysical.Location = new System.Drawing.Point(105, 21);
+            this.cmbSourcePhysical.Name = "cmbSourcePhysical";
+            this.cmbSourcePhysical.Size = new System.Drawing.Size(346, 21);
+            this.cmbSourcePhysical.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-1, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Source";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(-1, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Output";
+            // 
+            // btnStartPhysical
+            // 
+            this.btnStartPhysical.Location = new System.Drawing.Point(208, 87);
+            this.btnStartPhysical.Name = "btnStartPhysical";
+            this.btnStartPhysical.Size = new System.Drawing.Size(75, 23);
+            this.btnStartPhysical.TabIndex = 6;
+            this.btnStartPhysical.Text = "Start";
+            this.btnStartPhysical.UseVisualStyleBackColor = true;
+            this.btnStartPhysical.Click += new System.EventHandler(this.btnStartPhysical_Click);
+            // 
+            // txtOutputPhysical
+            // 
+            this.txtOutputPhysical.Location = new System.Drawing.Point(105, 48);
+            this.txtOutputPhysical.Name = "txtOutputPhysical";
+            this.txtOutputPhysical.Size = new System.Drawing.Size(346, 20);
+            this.txtOutputPhysical.TabIndex = 4;
+            // 
+            // btnBrowseOutputPhysical
+            // 
+            this.btnBrowseOutputPhysical.Location = new System.Drawing.Point(457, 46);
+            this.btnBrowseOutputPhysical.Name = "btnBrowseOutputPhysical";
+            this.btnBrowseOutputPhysical.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseOutputPhysical.TabIndex = 5;
+            this.btnBrowseOutputPhysical.Text = "Browse";
+            this.btnBrowseOutputPhysical.UseVisualStyleBackColor = true;
+            this.btnBrowseOutputPhysical.Click += new System.EventHandler(this.btnBrowseOutput_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(457, 20);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 5;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(558, 192);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(558, 333);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmbOutput);
             this.Controls.Add(this.cbDynamic);
-            this.Controls.Add(this.btnStart);
-            this.Controls.Add(this.btnBrowseOutput);
-            this.Controls.Add(this.btnBrowse);
-            this.Controls.Add(this.txtOutput);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtSourceFile);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "qemu-img-gui";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -173,8 +278,17 @@
         private System.Windows.Forms.Button btnBrowseOutput;
         private System.Windows.Forms.CheckBox cbDynamic;
         private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cmbOutput;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnStartPhysical;
+        private System.Windows.Forms.TextBox txtOutputPhysical;
+        private System.Windows.Forms.Button btnBrowseOutputPhysical;
+        private System.Windows.Forms.ComboBox cmbSourcePhysical;
+        private System.Windows.Forms.Button btnRefresh;
     }
 }
 
--- qemu-img-gui/qemu-img-gui/MainForm.cs
+++ qemu-img-gui/qemu-img-gui/MainForm.cs
@@ -8,16 +8,35 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Management;
 
 namespace qemu_img_gui
 {
     public partial class MainForm : Form
     {
         string qemuimgpath = @"qemu-img\qemu-img.exe";
+        List<ManagementObject> physicalDiskList = null;
 
         public MainForm()
         {
             InitializeComponent();
+            GetPhysicalDriveList();
+        }
+
+        private void GetPhysicalDriveList()
+        {
+            var query = new WqlObjectQuery("SELECT * FROM Win32_DiskDrive");
+            using (var searcher = new ManagementObjectSearcher(query))
+            {
+                physicalDiskList = searcher.Get()
+                                 .OfType<ManagementObject>()
+                                 .ToList();
+
+                var ds = physicalDiskList.Select(o => $"{o.Properties["DeviceID"].Value} | {o.Properties["Model"].Value} | {(Double.Parse(o.Properties["Size"].Value.ToString())/1073741824).ToString("N2")} GiB")
+                    .ToList();
+
+                cmbSourcePhysical.DataSource = ds;
+            }
         }
 
         private void btnBrowse_Click(object sender, EventArgs e)
@@ -30,12 +49,15 @@ namespace qemu_img_gui
         private void btnBrowseOutput_Click(object sender, EventArgs e)
         {
             var dialog = new SaveFileDialog();
-            dialog.FileName = BuildOutputName();
-            var filter = comboBox1.Text.Replace("vpc", "vhd");
+            dialog.FileName = BuildOutputName(dialog.FileName);
+            var filter = cmbOutput.Text.Replace("vpc", "vhd");
             dialog.Filter = "Virtual Disk | *." + filter;
             dialog.ShowDialog();
-            txtOutput.Text = dialog.FileName;
-            txtOutput.Text = BuildOutputName();
+            
+            if((sender as Button).Name == "btnBrowseOutput")
+                txtOutput.Text = BuildOutputName(dialog.FileName);
+            else if ((sender as Button).Name == "btnBrowseOutputPhysical")
+                txtOutputPhysical.Text = BuildOutputName(dialog.FileName);
         }
 
         private void btnStart_Click(object sender, EventArgs e)
@@ -54,16 +76,16 @@ namespace qemu_img_gui
                 return;
             }
 
-            if (comboBox1.SelectedIndex == -1)
+            if (cmbOutput.SelectedIndex == -1)
             {
                 MessageBox.Show("Please select output format.");
-                comboBox1.Focus();
+                cmbOutput.Focus();
                 return;
             }
 
             if (File.Exists(qemuimgpath))
             {
-                if (comboBox1.Text != "vpc" && comboBox1.Text != "vhdx")
+                if (cmbOutput.Text != "vpc" && cmbOutput.Text != "vhdx")
                 {
                     cbDynamic.Checked = false;
                 }
@@ -73,7 +95,7 @@ namespace qemu_img_gui
                     dynamic += "-o subformat=dynamic";
 
                 var command = String.Format("convert {0} -O {1} {2} {3}", 
-                    "\"" + txtSourceFile.Text.Trim()+ "\"", comboBox1.Text, dynamic, "\""+txtOutput.Text.Trim()+ "\"");
+                    "\"" + txtSourceFile.Text.Trim()+ "\"", cmbOutput.Text, dynamic, "\""+txtOutput.Text.Trim()+ "\"");
 
                 var process = new System.Diagnostics.Process();
                 process.StartInfo.FileName = qemuimgpath;
@@ -88,21 +110,21 @@ namespace qemu_img_gui
             }
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void cmbOutput_SelectedIndexChanged(object sender, EventArgs e)
         {
-            txtOutput.Text = BuildOutputName();
+            txtOutput.Text = BuildOutputName(txtOutput.Text);
         }
 
-        private string BuildOutputName()
+        private string BuildOutputName(string file)
         {
             string tmp = "";
             string tmpOutput = "";
-            if (!String.IsNullOrEmpty(txtOutput.Text))
-                tmpOutput = txtOutput.Text;
+            if (!String.IsNullOrEmpty(file))
+                tmpOutput = file;
             else
                 tmpOutput = txtSourceFile.Text;
 
-            tmp = Path.ChangeExtension(tmpOutput.Trim(), comboBox1.Text);
+            tmp = Path.ChangeExtension(tmpOutput.Trim(), cmbOutput.Text);
             tmp = tmp.Replace("vpc", "vhd");
             return tmp;
         }
@@ -110,6 +132,47 @@ namespace qemu_img_gui
         private static void myProcess_HasExited(object sender, System.EventArgs e)
         {
             MessageBox.Show("Done.");
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            GetPhysicalDriveList();
+        }
+
+        private void btnStartPhysical_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtOutputPhysical.Text.Trim()))
+            {
+                MessageBox.Show("Please browse output folder.");
+                txtOutputPhysical.Focus();
+                return;
+            }
+
+            if (File.Exists(qemuimgpath))
+            {
+                if (cmbOutput.Text != "vpc" && cmbOutput.Text != "vhdx")
+                {
+                    cbDynamic.Checked = false;
+                }
+
+                var dynamic = "";
+                if (cbDynamic.Checked)
+                    dynamic += "-o subformat=dynamic";
+
+                var command = String.Format("convert -f raw {0} -O {1} {2} {3}",
+                    cmbSourcePhysical.Text.Split('|')[0].Trim(), cmbOutput.Text, dynamic, "\"" + txtOutputPhysical.Text.Trim() + "\"");
+
+                var process = new System.Diagnostics.Process();
+                process.StartInfo.FileName = qemuimgpath;
+                process.StartInfo.Arguments = command;
+                process.EnableRaisingEvents = true;
+                process.Exited += new EventHandler(myProcess_HasExited);
+                process.Start();
+            }
+            else
+            {
+                MessageBox.Show("qemu-img.exe doesn't exist.");
+            }
         }
     }
 }
